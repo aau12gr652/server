4558372188c07dd2390fd08788c4604086732a15
Jonas Hansen <hr.jonas.hansen@gmail.com>
2012-06-18 15:19:44 +0200

    Nu kan den tælle

--- uep.cpp
+++ uep.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-	int Monte_Carlo = 1000;
+	int Monte_Carlo = 100000;
 	int gsize = 96;
 	int Over_Head = 300;
 	int symbol_size = 1;
@@ -18,7 +18,9 @@ int main()
 	foo.set_symbol_size(symbol_size);
 	serial_data Packet;
 	char file_name[10];
-	char* test_name	= "uepRandom";
+	char file_name2[20];
+	char* test_name = "uepRandom";
+	char* test_name2 = "uepRandomLayer";
 	char* file_type = "txt";
 
 	int layers = 2;
@@ -34,9 +36,15 @@ int main()
 	for (int u = 0; u < 3; u++)
 	{
 		std::ofstream statFileUEP;
+		std::ofstream LayerFile;
 		sprintf(file_name, "%s%d.%s",test_name,Gamma1[u],file_type);
+//		std::cout << "F1:" << file_name << std::endl;
 		statFileUEP.open(file_name);
 		statFileUEP << "%Generation, Packets, Layer 1, Layer 2" << std::endl;
+		sprintf(file_name2, "%s%d.%s",test_name2,Gamma1[u],file_type);
+		std::cout << "F2:" << file_name << std::endl;
+		LayerFile.open(file_name2);
+		LayerFile << "%Generation, Layer" << std::endl;
 		foo.set_layer_gamma(1, Gamma1[u]);
 		for (int n = 0; n < Monte_Carlo; n++)
 		{
@@ -49,6 +57,8 @@ int main()
 				Packet = foo.get_packet(); // Get a packet
 				poo.decode(&foo.payload_stamp, Packet); // Decode the packet
 
+				LayerFile << n << ", " << foo.payload_stamp.Layer_ID*1 << std::endl; // Report the layer
+
 				if (poo.is_layer_finish(1) == 1 || poo.is_layer_finish(2) == 1 || poo.is_layer_finish(3) == 1)
 				{
 					statFileUEP << n << ", " << Packet_Number+1;
@@ -60,10 +70,11 @@ int main()
 					break; // No need to continue, since the data is all decoded
 			}
 			//std::cout << "Required packets (UEP): " << Packet_Number + 1 << std::endl;
-			if (n%500 == 0)
+			if (n%5000 == 0)
 					std::cout << "Generation " << n << " did complete, with gamma1 = " << Gamma1[u]*1 << std::endl;
 		}
 		statFileUEP.close();
+		LayerFile.close();
 	}
 	free(RandomData); // Free the random data
     return 0;
